318728fac8521df4fee5d7df327a1c631518327f
Paweł <pawel.wankowski@wp.pl>
2022-01-04 21:08:48 +0100

    feature(pwa): add new microservice to WeatherAPI

--- WheatherApplication/src/main/java/com/example/WheatherApplication/WheatherApplication.java
+++ WheatherApplication/src/main/java/com/example/WheatherApplication/WheatherApplication.java
@@ -2,8 +2,10 @@ package com.example.WheatherApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class WheatherApplication {
 
 	public static void main(String[] args) {
