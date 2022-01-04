ff64e566f4bcfc741f2440fa5d18df6400d8a8ca
Paweł <pawel.wankowski@wp.pl>
2022-01-04 19:34:44 +0100

    Init repo

---
+++ WheatherApplication/src/test/java/com/example/WheatherApplication/WheatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WheatherApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
