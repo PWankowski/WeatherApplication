7e9ba2eb303f00818b6dc8214ea1ad68f23a2499
Paweł <pawel.wankowski@wp.pl>
2022-01-09 17:57:47 +0100

    feature(pwa): added ConfigService and modified config class in Client1 to get url address and service name for weather feedback

--- Client1/src/main/java/com/example/Client1/ConfigController.java
+++ Client1/src/main/java/com/example/Client1/ConfigController.java
@@ -8,10 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/config")
 public class ConfigController {
 
+    private  final ConfigService configService;
+
+    public ConfigController(ConfigService configService) {
+        this.configService = configService;
+    }
 
     @GetMapping
     public Config getConfig(){
-        return  new Config("test_api_key");
+      return   configService.getConfig();
     }
 
 
@@ -21,20 +26,30 @@ public class ConfigController {
 
 class Config {
 
-    private String apiKey;
+    private String url;
+    private String serviceName;
 
-    public Config(String apiKey) {
-        this.apiKey = apiKey;
+    public Config(String url, String serviceName) {
+        this.url = url;
+        this.serviceName = serviceName;
     }
 
     public Config() {
     }
 
-    public String getApiKey() {
-        return apiKey;
+    public String getUrl() {
+        return url;
     }
 
-    public void setApiKey(String apiKey) {
-        this.apiKey = apiKey;
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
     }
 }
