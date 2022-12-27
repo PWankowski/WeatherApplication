59972107e87c8e448df6297267cfe7d41053ae6a
Paweł <pawel.wankowski@wp.pl>
2022-12-27 15:25:25 +0100

    new Api Keys for Weather Api

--- Client1/src/main/java/com/example/Client1/ConfigService.java
+++ Client1/src/main/java/com/example/Client1/ConfigService.java
@@ -13,10 +13,10 @@ public class ConfigService {
     public List<Config> getConfig(){
 
         Config openWeatherApiSource1 = new Config(
-                "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=baa6ece140be0985d8bf8766fa381d1d&units=metric",
+                "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=4a2a3b81d4817483a66f50afe4ca3757&units=metric",
                 "openWeatherMap");
         Config openWeatherApiSource2 = new Config(
-                "http://api.weatherstack.com/current?access_key=17ff09ba764b15dc96f5f8436421b30f&query=%s",
+                "http://api.weatherstack.com/current?access_key=4e804229182e92fe0f3b529babf9eec8&query=%s",
                 "weatherStack");
 
         return Arrays.asList(openWeatherApiSource1,openWeatherApiSource2);
